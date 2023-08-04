aea8c968b8a09c9906725f92c85f0c6e8eacdaa2
epw1624 <112768709+epw1624@users.noreply.github.com>
2023-08-03 19:51:26 -0700

    implement jpg to png conversions

---
+++ jpg_to_png.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    if len(args) < 1:
+        raise Exception("Must select at least one file")
+    
+    for arg in args[1:]:
+        img = Image.open(arg)
+        new_name = arg.replace('jpg', 'png')
+        img.save(new_name)
+        img.close()
+
+
